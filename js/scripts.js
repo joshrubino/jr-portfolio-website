2b87ea037844eb47ef4730c30339433f0a64494f
joshrubino <joshrubino@gmail.com>
2021-05-03 06:52:20 -0400

    Update scripts.js - fixed responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ console.log("Hi. I'm Josh. Welcome to my site!");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
